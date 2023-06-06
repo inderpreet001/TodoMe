be96bd38eac4b15b70e7df40442597b9ff35828a
Inderpreet <login.inderpreet@gmail.com>
2023-06-06 13:19:52 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,92 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import Image from "next/image";
+import  "../styles/home.module.css";
+import { Button, Container, Form, Stack } from "react-bootstrap";
+import { useState } from "react";
 
 export default function Home() {
+  const [todo, setTodo] = useState([]);
+
+  const handleAddTodo = () => {
+    const value = document.getElementById("todo-input").value;
+
+    if(value!=""){
+      const currentDate = new Date().toLocaleDateString();
+      setTodo([...todo, { id: todo.length + 1, text: value, completed: false,createdOn:currentDate }]);
+    document.getElementById("todo-input").value = ""
+    }
+    
+  };
+
+  const handleTodoCheck = (index) => {
+    console.log(index);
+    const newTodo = todo.map((item, i) => {
+      if (i + 1 === index) {
+        console.log();
+        return { id: item.id, text: item.text, completed: !item.completed,createdOn:item.createdOn };
+      } else {
+        // The rest haven't changed
+        return item;
+      }
+    });
+
+    setTodo(newTodo);
+    console.log(todo);
+  };
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="d-flex justify-content-center mt-4">
+        <h1>TodoMe</h1>
+      </div>
+
+      <Container className="mt-3">
+        <div className="d-flex justify-space-between mb-4">
+          <Form.Control
+            type="text"
+            placeholder="Enter Todo here"
+            className="me-5"
+            id="todo-input"
+          />
+          <Button onClick={(e) => handleAddTodo()}>Add</Button>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        {todo != []
+          ? todo.map((item, index) => (
+              <Stack gap={4} direction="horizontal">
+                {item.completed ? (
+                  <div className="d-flex justify-content-between todo-item mb-2 align-items-center">
+                    <div className="d-flex flex-column">
+                    <strike><b>{item.text}</b></strike>
+                    <span style={{fontSize:"11px"}}>{item.createdOn}</span>
+                    </div>
+                    <Form.Check
+                    className="ms-3"
+                      type="checkbox"
+                      id={`default-${index}`}
+                      checked={item.completed}
+                      onChange={(e) => handleTodoCheck(item.id)}
+                    />
+                  </div>
+                ) : (
+                  <div className="d-flex justify-content-between todo-item mb-2 align-items-center">
+                    <div className="d-flex flex-column">
+                    <span><b>{item.text}</b></span>
+                    <span style={{fontSize:"11px"}}>{item.createdOn}</span>
+                    </div>
+                    <Form.Check
+                     className="ms-3"
+                      type="checkbox"
+                      id={`default-${index}`}
+                      checked={item.completed}
+                      onChange={(e) => handleTodoCheck(item.id)}
+                    />
+                  </div>
+                )}
+              </Stack>
+            ))
+          : ""}
+      </Container>
+    </>
+  );
 }
